6e13de9a0fe61d35b58a0b882cccfc81276c4481
Judah Ben <judahben149@gmail.com>
2024-07-05 22:39:37 +0100

    Added strings and constants

---
+++ app/src/main/java/com/shashank/cashlessatm/utils/Constants.kt
@@ -0,0 +1,11 @@
+package com.shashank.cashlessatm.utils
+
+object Constants {
+    const val SETTINGS = "SETTINGS"
+    const val DEFAULT = "DEFAULT"
+
+    // Network
+    const val DEFAULT_IP_ADDRESS = "206.71.17.21" // 206.71.17.21
+    const val DEFAULT_PORT = 443 //443
+    const val DEFAULT_TIMEOUT = 60000
+}
