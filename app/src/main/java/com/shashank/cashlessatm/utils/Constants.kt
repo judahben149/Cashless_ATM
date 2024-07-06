8c6bdde213c8e372d26b2b1fbea3b86a1ce13ca6
Judah Ben <judahben149@gmail.com>
2024-07-06 12:23:17 +0100

    Added constants

--- app/src/main/java/com/shashank/cashlessatm/utils/Constants.kt
+++ app/src/main/java/com/shashank/cashlessatm/utils/Constants.kt
@@ -4,6 +4,11 @@ object Constants {
     const val SETTINGS = "SETTINGS"
     const val DEFAULT = "DEFAULT"
 
+    // Pref Keys
+    const val KEY_IP_ADDRESS = "KEY_IP_ADDRESS"
+    const val KEY_PORT = "KEY_PORT"
+    const val KEY_TIMEOUT = "KEY_TIMEOUT"
+
     // Network
     const val DEFAULT_IP_ADDRESS = "206.71.17.21" // 206.71.17.21
     const val DEFAULT_PORT = 443 //443
