1b64a50ad933a5620032bd96779bc2f305813baa
Judah Ben <judahben149@gmail.com>
2024-07-06 12:35:34 +0100

    Added Usecase class

---
+++ app/src/main/java/com/shashank/cashlessatm/domain/usecase/KeyExchangeUseCase.kt
@@ -0,0 +1,10 @@
+package com.shashank.cashlessatm.domain.usecase
+
+import com.shashank.cashlessatm.domain.model.IsoChannelMessage
+
+class KeyExchangeUseCase() {
+
+    fun downloadKeys(channelMessage: IsoChannelMessage) {
+
+    }
+}
