e783c74d73bf29d03bf985edba78bd1d07c2b0e3
Judah Ben <judahben149@gmail.com>
2024-07-16 14:30:07 +0100

    Added custom channel and modified service class

--- app/src/main/java/com/shashank/cashlessatm/data/network/iso/WorldPayIsoServiceImpl.kt
+++ app/src/main/java/com/shashank/cashlessatm/data/network/iso/WorldPayIsoServiceImpl.kt
@@ -1,6 +1,8 @@
 package com.shashank.cashlessatm.data.network.iso
 
+import android.content.Context
 import com.shashank.cashlessatm.data.network.tcpip.WorldPayChannel
+import com.shashank.cashlessatm.data.network.tcpip.WorldpayCustomChannel
 import com.shashank.cashlessatm.domain.SessionManager
 import com.shashank.cashlessatm.domain.interfaces.IsoService
 import com.shashank.cashlessatm.domain.model.IsoChannelMessage
@@ -13,12 +15,19 @@ import org.jpos.iso.IFE_NUMERIC
 import org.jpos.iso.ISOField
 import org.jpos.iso.ISOMsg
 import org.jpos.iso.ISOUtil
+import org.jpos.iso.packager.GenericPackager
+import org.jpos.util.Logger
+import org.jpos.util.SimpleLogListener
+import java.io.InputStream
 import java.util.BitSet
 import java.util.Date
 import javax.inject.Inject
+import com.shashank.cashlessatm.utils.logs.Logger as myLogger
 
 class WorldPayIsoServiceImpl @Inject constructor(
     private val channel: WorldPayChannel,
+    private val customChannel: WorldpayCustomChannel,
+    private val context: Context,
     private val packager: WorldPayPackager,
     private val sessionManager: SessionManager,
     private val isoUtils: IsoUtils
@@ -26,31 +35,102 @@ class WorldPayIsoServiceImpl @Inject constructor(
 
     override suspend fun downloadKeys(channel: Channel<IsoChannelMessage>): Boolean {
         downloadMasterKey(channel)
+//        unpack()
         return false
     }
 
-    private fun downloadMasterKey(channel: Channel<IsoChannelMessage>) {
+    private fun downloadMasterKey(channelEvent: Channel<IsoChannelMessage>) {
         val date = Date()
         val stan = isoUtils.getNextStan()
 
         val tmkRequest = ISOMsg()
         tmkRequest.packager = packager
 
+        val logger = Logger()
+        logger.addListener(SimpleLogListener(System.out))
+        packager.setLogger(logger, "debug")
+
+        tmkRequest.mti = "0800"
+
         tmkRequest.apply {
-            set("0.1", "MA.")
-            set("0.2", "0800")
-            set("0.3", "0800") // Primary Bitmap- To replace with calculated Bitmap after all fields are set
-            set("1", "0000010000000000000000000000000000000000000000000000000000000000") // Secondary bitmap
             set("7", timeAndDateFormatter.format(date))
             set("11", stan)
             set("70", "301") //0301, 801
-            // To set - Field 115 - Has subfields
+            set("115.0", "E000000000000000") //Bitmap showing that fields 1, 2, 3 are set
+            set("115.1", "0000")
+            set("115.2", "123")
+            set("115.3", "012345678")
         }
 
+        tmkRequest.dump(System.out, "")
+        val packed = tmkRequest.pack()
+        val bitmap = tmkRequest.getValue(-1)
+        myLogger.log("BITMAP ----> $bitmap")
 
-        tmkRequest.pack()
+        val unpacked = ISOMsg()
+        unpacked.packager = packager
+        unpacked.unpack(packed)
+        val bitmap2 = tmkRequest.getValue(-1)
+        myLogger.log("BITMAP UNPACKED----> $bitmap2")
+
+        customChannel.apply {
+            connect()
+            myLogger.log("Is channel connected? - $isConnected")
+            send(packed)
+        }
+
+        val response = customChannel.receive()
+        customChannel.disconnect()
+        response.dump(System.out, "")
+
+    }
+
+    private fun returnField0packager(): GenericPackager {
+        val packagerInputStream: InputStream = context.assets.open("worldpay_field0.xml")
+        return GenericPackager(packagerInputStream)
+    }
+
+    private fun buildField0(): ByteArray {
+        val field0 = ISOMsg()
+        field0.packager = returnField0packager()
+
+        field0.apply {
+            set("1", "MA.")
+            set("2", "0800")
+            set("3", "1000001000100000000000000000000000000000000000000000000000000000")
+        }
+
+        return field0.pack()
+    }
+
+    private fun unpack() {
+
+        val dump = "08008220000000000000040000000000001004011333551333550160000ED2C5E82000000000000000F1F0F2"
 
 
+        val b = ISOUtil.hex2byte(dump.replace(" ", ""))
+        println(ISOUtil.hexdump(b))
+
+//        val packagerInputStream: InputStream = context.assets.open("worldpay.xml")
+//        val packager = GenericPackager("worldpay.xml")
+        val m = ISOMsg()
+        val logger = Logger()
+        logger.addListener(SimpleLogListener(System.out))
+        m.packager = packager
+        packager.setLogger(logger, "debug")
+        m.unpack(b)
+        m.dump(System.out, "")
+
+        val m1 = ISOMsg()
+        m1.packager = packager
+        m1.unpack(m.pack())
+        m.dump(System.out, "")
+
+        if (!b.contentEquals(m.pack())) {
+            println("Pack/Unpack differs")
+            println(ISOUtil.hexdump(b))
+            println(ISOUtil.hexdump(m.pack()))
+        }
     }
 
     private fun constructField0 (): ByteArray? {
---
+++ app/src/main/java/com/shashank/cashlessatm/data/network/tcpip/WorldpayCustomChannel.kt
@@ -0,0 +1,121 @@
+package com.shashank.cashlessatm.data.network.tcpip
+
+import com.shashank.cashlessatm.domain.SessionManager
+import org.jpos.iso.BaseChannel
+import org.jpos.iso.ISOException
+import org.jpos.iso.ISOMsg
+import org.jpos.iso.ISOUtil
+import org.jpos.iso.packager.GenericPackager
+import org.jpos.util.Logger
+import java.io.BufferedReader
+import java.io.ByteArrayOutputStream
+import java.io.IOException
+import java.io.InputStream
+import java.net.SocketException
+import java.util.BitSet
+import java.util.concurrent.TimeoutException
+import javax.inject.Inject
+import kotlin.jvm.Throws
+
+class WorldpayCustomChannel @Inject constructor(
+    ipAddress: String,
+    port: Int,
+    private val packager: GenericPackager?,
+    sessionManager: SessionManager
+) : BaseChannel(ipAddress, port, packager) {
+
+    private var myHeader = ""
+
+    init {
+        // SSL setup if required by Worldpay
+        val sslSocketFactory = TcpSslConnection()
+        sslSocketFactory.setPassword("")
+        sslSocketFactory.setKeyStore("")
+        setSocketFactory(sslSocketFactory)
+        setTimeOut(sessionManager.getTimeout())
+    }
+
+    @Throws(IOException::class)
+    override fun sendMessageLength(len: Int) {
+//        serverOut.write(len shr 8)
+//        serverOut.write(len)
+        serverOut.write(0) // The length is already concatenated in the custom header being constructed and sent
+    }
+
+    @Throws(IOException::class, ISOException::class)
+    override fun getMessageLength(): Int = -1
+
+
+    override fun sendMessageHeader(m: ISOMsg, len: Int) {
+        try {
+            m.packager = packager
+            val messageOriginator = "BT"
+            val messageLength = m.pack().size
+            val echoData = "100000100010000"
+
+            myHeader = myHeader + messageOriginator + messageLength + echoData
+
+            print("HEADER >>>>>> $header")
+
+            logRawData("Header sent --> ", myHeader.toByteArray(Charsets.UTF_8))
+            serverOut.write(myHeader.toByteArray(Charsets.UTF_8))
+        } catch (e: ISOException) {
+            print(e.message)
+        }
+    }
+
+    /**
+     * * @param header Hex representation of header
+     */
+//    override fun setHeader(header: String) {
+//        super.setHeader(
+//            ISOUtil.hex2byte(myHeader.toByteArray(Charsets.UTF_8), 0, myHeader.length)
+//        )
+//    }
+
+    override fun receive(): ISOMsg {
+        return try {
+            val rawData = readRawData(serverIn)
+            logRawData("Received Message -->", rawData)
+
+            // Remove header structure from stream
+
+            val message = ISOMsg()
+            message.packager = packager
+            message.unpack(rawData)
+            message
+//        return super.receive() // To try too
+        } catch (e: SocketException) {
+            println("SocketException: ${e.message}")
+            throw ISOException("Error receiving message", e)
+        }
+    }
+
+    private fun readRawData(input: InputStream): ByteArray {
+        val baos = ByteArrayOutputStream()
+        val buffer = ByteArray(4096)
+        var bytesRead: Int
+        return try {
+            while (input.read(buffer).also { bytesRead = it } != -1) {
+                baos.write(buffer, 0, bytesRead)
+            }
+            baos.toByteArray()
+        } catch (e: SocketException) {
+            println("SocketException while reading: ${e.message}")
+            baos.toByteArray() // Return whatever has been read so far
+        }
+    }
+
+    private fun logRawData(prefix: String, data: ByteArray) {
+        val hexString = data.joinToString(" ") { String.format("%02X", it) }
+        println("$prefix Raw Data: $hexString")
+    }
+
+    private fun setTimeOut(timeout: Int) {
+        try {
+            super.setTimeout(timeout)
+        } catch (ex: SocketException) {
+            throw TimeoutException("Failed to apply timeout (${timeout}s)...")
+        }
+    }
+}
